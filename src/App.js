89c7292687a2d1ad360798ff4e6630b79ce9d446
negi1232 <ryosuke.geek@gmail.com>
2022-09-10 12:00:34 +0900

    etherとreactを接続

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 import contract from './contracts/NFTCollectible.json';
 import { ethers } from 'ethers';
 
-const contractAddress = "0x355638a4eCcb777794257f22f50c289d4189F245";
+const contractAddress = "0x3392C6d657360d90179dAce208FF527Ba230A2fB";
 const abi = contract.abi;
 
 function App() {
+  const [mint_mode, setText_1] = useState("");//購入する巻のテキスト保存
+  
+  const [Selling_price, setText_2] = useState("");//販売価格のテキスト保存
+  const [To, setText_3] = useState("");//販売相手のアドレスを保存
+  const [Token_id, setText_4] = useState("");//販売するNFTのトークンidを保存
+
+  const [Buy_price, setText_5] = useState("");//購入価格のテキスト保存
+  const [Buy_Token_id, setText_6] = useState("");//購入するNFTのトークンidを保存
+  
+  
+
+  
 
   const [currentAccount, setCurrentAccount] = useState(null);
 
@@ -47,6 +59,7 @@ function App() {
     }
   }
 
+  
   const mintNftHandler = async () => {
     try {
       const { ethereum } = window;
@@ -57,12 +70,125 @@ function App() {
         const nftContract = new ethers.Contract(contractAddress, abi, signer);
 
         console.log("Initialize payment");
-        let nftTxn = await nftContract.mintNFTs(1, { value: ethers.utils.parseEther("0.01") });
+        let nftTxn = await nftContract.mint(mint_mode, { value: ethers.utils.parseEther("0.05") });
 
         console.log("Mining... please wait");
         await nftTxn.wait();
 
-        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+        console.log(mint_mode);
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const get_mftHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        
+        var items =[];
+        for (let i = 1; i <= 5; i++){
+          const res = await nftContract._auth(i);
+          if(res===true){
+            items.push(i)
+          }
+        }
+        console.log(items);
+        
+        console.log("Mining... please wait");
+        
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const approveHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        
+        console.log(Selling_price);
+        const res = await nftContract.approve(To,Token_id,ethers.utils.parseEther(Selling_price));
+          
+        
+        console.log("Mining... please wait");
+        
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const tradeHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        
+        console.log(Selling_price);
+        const res = await nftContract.trade(Token_id,{ value: ethers.utils.parseEther(Buy_price) });
+          
+        
+        console.log("Mining... please wait");
+        
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const withdrawHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        
+        console.log(Selling_price);
+        const res = await nftContract.withdraw();
+          
+        
+        console.log("Mining... please wait");
+        
 
       } else {
         console.log("Ethereum object does not exist");
@@ -76,28 +202,91 @@ function App() {
   const connectWalletButton = () => {
     return (
       <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
-        Connect Wallet
+        ログイン
       </button>
     )
   }
 
   const mintNftButton = () => {
     return (
+      
       <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
-        Mint NFT
+        NFTを発行(巻数を指定)
       </button>
     )
   }
 
-  useEffect(() => {
-    checkWalletIsConnected();
-  }, [])
+  const get_nfts = () => {
+    return (
+      
+      <button onClick={get_mftHandler} className='cta-button mint-nft-button'>
+        所持しているNFTから閲覧可能な巻を確認
+
+      </button>
+    )
+  }
+
+  const approve = () => {
+    return (
+      
+      <button onClick={approveHandler} className='cta-button mint-nft-button'>
+        販売
+
+      </button>
+    )
+  }
+
+  const trade = () => {
+    return (
+      
+      <button onClick={tradeHandler} className='cta-button mint-nft-button'>
+        購入
+
+      </button>
+    )
+  }
+
+  const withdraw = () => {
+    return (
+      
+      <button onClick={withdrawHandler} className='cta-button mint-nft-button'>
+        払い出し
+
+      </button>
+    )
+  }
 
   return (
     <div className='main-app'>
       <h1>Scrappy Squirrels Tutorial</h1>
       <div>
-        {currentAccount ? mintNftButton() : connectWalletButton()}
+        
+        閲覧系の関数
+        <br/>
+        <input type="text" value={mint_mode} onChange={(event) => setText_1(event.target.value)}/>巻
+        <br/>
+        {mintNftButton()}
+        <br/><br/>
+        {connectWalletButton()}
+        <br/><br/>
+        {get_nfts()}
+        <br/><br/>
+        譲渡系の関数<br/>
+        
+        <input type="text" value={Selling_price} onChange={(event) => setText_2(event.target.value)}/>ether 販売価格<br/>
+        <input type="text" value={To} onChange={(event) => setText_3(event.target.value)}/>販売する相手のアドレス<br/>
+        <input type="text" value={Token_id} onChange={(event) => setText_4(event.target.value)}/>Token_id<br/>
+        {approve()}<br/><br/>
+
+        <input type="text" value={Buy_price} onChange={(event) => setText_5(event.target.value)}/>ether 購入価格<br/>{/* 自動取得したい */}
+        <input type="text" value={Buy_Token_id} onChange={(event) => setText_6(event.target.value)}/>Token_id<br/>
+        {trade()}<br/><br/>
+
+        {withdraw()}
+
+
+        
+
       </div>
     </div>
   )
