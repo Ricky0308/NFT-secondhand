e319c6f3e43716f25eb81dc7ff5c21f73e63a5db
門田凌典 <green0308@softbank.ne.jp>
2022-09-13 06:46:09 +0900

    save

--- backend/contents/models.py
+++ backend/contents/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contents(models.Model):
+    book_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=150)
     content = models.FileField(upload_to='uploads/%Y/%m/%d/', default='samples/sample_content.pdf')
     cover = models.ImageField(upload_to='uploads/%Y/%m/%d/', default='samples/sample_cover.jpeg')
