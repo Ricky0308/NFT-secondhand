dde1e9e79213f3c29bb324fb4795b6a32532450b
yano0 <yanonay3@gmail.com>
2022-09-12 12:29:04 +0900

    routeの追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,25 @@ import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Assignment from "./components/assignment";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <AppBar position="static" style={{ backgroundColor: "#534F69" }}>
+       <AppBar position="static" style={{ backgroundColor: "#534F69" }}>
         <Typography component="h1" variant="h44" margin={2}>
           Secondly
         </Typography>
       </AppBar>
-      <Assignment />
+          <BrowserRouter>
+            <Routes>
+              <Route path={`/`} element={<Assignment />} />
+              {/* <Route path={`*`} element={<Error />} /> */}
+            </Routes>
+          </BrowserRouter>
+
+     
+      
     </div>
   );
 }
