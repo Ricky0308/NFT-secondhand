1038dfb289b5ba49170936744b9fc55790638d66
門田凌典 <green0308@softbank.ne.jp>
2022-09-13 13:49:15 +0900

    assgnmentでurlパラメータを取れるように

--- src/pages/bookshell.jsx
+++ src/pages/bookshell.jsx
@@ -2,22 +2,20 @@ import React from "react";
 import { BookCard } from "../components/bookcard";
 import { useEffect, useState } from "react";
 import { Box, Container } from "@mui/system";
-
-import contract from "../contracts/abi.json";
-import { ethers } from "ethers";
-const contractAddress = "0xb9c35E386528047Aaa810F6E3d2521a202E7872F";
-const abi = contract.abi;
+import { get_nftHandler } from "../functions/get_nftHandler";
 
 const AddressToContentsId = () => {
     return [1, 100]
 }
 
 export default function Bookshell(){
-
-    const [bookIdArray, setBookIdArray] = useState([]);
+    const [ bookIdArray, setBookIdArray ] = useState([]);
     useEffect(()=>{
         setBookIdArray(AddressToContentsId());
-
+        get_nftHandler()
+            .then((items) => {
+                setBookIdArray(items);
+            })
     }, [])
 
     return(
@@ -32,7 +30,7 @@ export default function Bookshell(){
                     sx={{
                     }}
                 >
-                    あんたの本棚
+                    あんたの本棚 
                 </Box>
                 <Container
                     sx={{
@@ -52,38 +50,3 @@ export default function Bookshell(){
 }
 
 
-
-
-
-
-const get_mftHandler = async () => {
-    var items = [];
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const nftContract = new ethers.Contract(contractAddress, abi, signer);
-
-        console.log("Initialize payment");
-
-        for (let i = 1; i <= 5; i++) {
-          const res = await nftContract._auth(i);
-          if (res === true) {
-            items.push(i);
-          }
-        }
-
-        // setText_7(items);
-        console.log(items);
-
-        console.log("Mining... please wait");
-        return items;
-      } else {
-        console.log("Ethereum object does not exist");
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
