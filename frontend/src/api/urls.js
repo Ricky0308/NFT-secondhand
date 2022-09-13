48587dabcba12da18f18ec0be6e208fc2b78d3b4
Ricky0308 <87261710+Ricky0308@users.noreply.github.com>
2022-09-13 14:00:00 +0900

    Delete frontend directory

--- frontend/src/api/urls.js
+++
@@ -1,1 +0,0 @@
-export const baseAPIUrl = "http://localhost:8000/"
--- frontend/src/pages/bookshell.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { BookCard } from "../components/bookcard";
-
-
-export const Bookshell = () => {
-    return(
-        <BookCard/>
-    )
-}
